aa4b0b1f65397d99c07e84cd53333ca5ae69ed54
Galen Holt <53928785+galenholt@users.noreply.github.com>
2023-05-29 17:02:04 +1000

    cleaning up scaling functions, adding ability to save different ways and force zeros

--- scenario_creation/scaling_functions.R
+++ scenario_creation/scaling_functions.R
@@ -62,29 +62,32 @@ get_q_unique <- function(vals, q_perc) {
 get_scalings <- function(sdl_runoff_scenarios) {
   # Stack
   stackdf <- sdl_runoff_scenarios %>%
-    mutate(sdl = stringr::str_extract(path, "SS[0-9]+")) %>%
-    select(sdl, Year, Month, Day, starts_with('Sim')) %>%
-    pivot_longer(cols = starts_with('Sim'),
+    dplyr::mutate(sdl = stringr::str_extract(path, "SS[0-9]+")) %>%
+    # We only need Month, not year or day, and we need it to not be numeric
+    dplyr::mutate(Date = lubridate::make_date(Year, Month, Day),
+                  Month = lubridate::month(Date, label = TRUE)) %>%
+    dplyr::select(sdl, Month, tidyselect::starts_with('Sim')) %>%
+    tidyr::pivot_longer(cols = tidyselect::starts_with('Sim'),
                  names_to = 'scenario', values_to = 'runoff')
 
   # Quantile means
   q_s <- stackdf %>%
-    group_by(scenario, Month) %>%
-    summarize(qmean = get_qmean(runoff), .groups = 'drop') %>%
+    dplyr::group_by(scenario, Month) %>%
+    dplyr::summarise(qmean = get_qmean(runoff), .groups = 'drop') %>%
     # reframe is the new way, but needs dplyr 1.1 which breaks lots of the functions
     # reframe(qmean = get_qmean(runoff)) %>%
     tidyr::unnest(cols = qmean)
 
   # Get the relative change
   q_s <- q_s %>%
-    # group_by(scenario, Month) %>%
+    # dplyr::group_by(scenario, Month) %>%
     baseline_compare(compare_col = 'scenario',
                      base_lev = 'SimR0',
                      values_col = 'mean',
                      group_cols = c('Month', 'quantile'),
                      comp_fun = `/`) %>%
-    ungroup() %>%
-    rename(relative_change = `/_mean`)
+    dplyr::ungroup() %>%
+    dplyr::rename(relative_change = `/_mean`)
 }
 
 
@@ -103,38 +106,44 @@ get_matched_ranks <- function(hydro, model, monthly = TRUE) {
   # We need the minimum number across hydro and all scenarios (which may have
   # different numbers of duplicates.)
 
+  # We only need Month, not year or day, and we need it to not be numeric
+  model <- model %>%
+    dplyr::mutate(Date = lubridate::make_date(Year, Month, Day),
+                  Month = lubridate::month(Date, label = TRUE))
+
+
   # Allow monthly or overall matching
   # if (monthly) {monthgroup <- rlang::quo(Month)} else {monthgroup = NULL}
 
   if (monthly) {monthgroup <- "Month"} else {monthgroup = NULL}
 
   uniquemodel <- model %>%
-    pivot_longer(cols = starts_with('Sim')) %>%
-    group_by(name, across(any_of(monthgroup))) %>%
-    summarise(nunique = n_distinct(value), .groups = 'drop')
+    tidyr::pivot_longer(cols = tidyselect::starts_with('Sim')) %>%
+    dplyr::group_by(name, across(any_of(monthgroup))) %>%
+    dplyr::summarise(nunique = dplyr::n_distinct(value), .groups = 'drop')
 
   uniquehydro <- hydro %>%
-    group_by(across(any_of(monthgroup))) %>%
-    summarise(nunique = n_distinct(value), .groups = 'drop')
+    dplyr::group_by(across(any_of(monthgroup))) %>%
+    dplyr::summarise(nunique = dplyr::n_distinct(value), .groups = 'drop')
 
   fewest <- min(min(uniquehydro$nunique),
                 min(uniquemodel$nunique))
 
   # Rank the hydrograph
   hydro <- hydro %>%
-    group_by(across(any_of(monthgroup))) %>%
-    mutate(ranks = get_q_unique(value, 1/fewest)) %>%
-    ungroup()
+    dplyr::group_by(across(any_of(monthgroup))) %>%
+    dplyr::mutate(ranks = get_q_unique(value, 1/fewest)) %>%
+    dplyr::ungroup()
 
   model_rankvals <- model %>%
-    pivot_longer(cols = starts_with('Sim'), names_to = 'scenario') %>%
-    group_by(scenario, across(any_of(monthgroup))) %>%
-    mutate(ranks = get_q_unique(value, 1/fewest)) %>%
-    group_by(scenario, across(any_of(monthgroup)), ranks) %>%
-    summarise(mod_vals = median(value), .groups = 'drop')
+    tidyr::pivot_longer(cols = tidyselect::starts_with('Sim'), names_to = 'scenario') %>%
+    dplyr::group_by(scenario, across(any_of(monthgroup))) %>%
+    dplyr::mutate(ranks = get_q_unique(value, 1/fewest)) %>%
+    dplyr::group_by(scenario, across(any_of(monthgroup)), ranks) %>%
+    dplyr::summarise(mod_vals = median(value), .groups = 'drop')
 
   # replace (really, add another column)
-  rank_hydro <- left_join(hydro, model_rankvals, by = c('ranks', monthgroup))
+  rank_hydro <- dplyr::left_join(hydro, model_rankvals, by = c('ranks', monthgroup))
 
   return(rank_hydro)
 }
@@ -151,6 +160,9 @@ get_matched_ranks <- function(hydro, model, monthly = TRUE) {
 #' @param regression if NULL, just returns q-q. If 'linear', linear regression over the last `reg_rank_prop`, if log, data is transformed to log scale for the regression and back-transformed to return values
 #' @param reg_rank_prop proportion of data to use a regression fill on. Typically some small % of the bottom, but if 1, it will just regress the whole thing. I'm not going to bother writing the switches to kill the q-q in that case, since it's unlikely we'll use it. So it will build the q-q and then replace all the data.
 #' @param lower_limit lower limit of hydrograph values we trust. Typically 1. Forms the 0-cutoff for log regressions, but also the data range to do linear regression.
+#' @param savedata logical- save the csvs into scenario folders
+#' @param saverds logical- save rds files for each gauge
+#' @param returnR logical- return the output to R session, or if FALSE, return NULL invisibly
 #'
 #' @return
 #' @export
@@ -187,10 +199,7 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
     return(NULL)
   }
 
-  rlang::inform(glue::glue('starting gauge {gaugename} in SDL {sdl_name}\n'))
-
-  # if they are level gauges, return NULL- the scaling doesn't make sense
-  if (gaugename %in% gauge_cats$level) {return(NULL)}
+  # rlang::inform(glue::glue('starting gauge {gaugename} in SDL {sdl_name}\n'))
 
   # Clean the data and scale the scenario- we only need one of the all_sdl_scenario_list
   datalist <- clean_to_scale(gaugedata = gaugedata, gaugename = gaugename,
@@ -202,16 +211,16 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
 
   # qq all the data- this is cleaner and easier to check than doing it piecewise
   qq_gauge <- datalist$gaugedata %>%
-    group_by(Month) %>%
+    dplyr::group_by(Month) %>%
     # get quantiles- make a dummy so NA quantiles exist and get join-crossed with scenarios
-    mutate(quantile = get_q(value, q_perc = 0.02)) %>%
-    ungroup() %>%
+    dplyr::mutate(quantile = get_q(value, q_perc = 0.02)) %>%
+    dplyr::ungroup() %>%
     # join to correct sdl unit for the scalings
-    left_join(datalist$model_with_scaling,
+    dplyr::left_join(datalist$model_with_scaling,
               by = c('Month', 'quantile'),
               multiple = 'all') %>% # Says it's OK to duplicate rows x scenarios
     # get the adjusted levels
-    mutate(adj_val = value*relative_change) %>%
+    dplyr::mutate(adj_val = value*relative_change) %>%
     # Just the needed cols
     dplyr::select(scenario, site, Date, adj_val)
 
@@ -222,11 +231,11 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
 
     # For the regression, we toss everything below lower_limit, so we need to know how many ranks are above this- those below shouldn't be included in the data to feed to the regression.
     rank_summary <- rank_data %>%
-      filter(scenario == 'SimR0') %>%
-      group_by(Month) %>%
-      filter(value >= lower_limit) %>%
-      summarise(mingoodrank = min(ranks), maxrank = max(ranks)) %>%
-      mutate(nrankbottom = ceiling((maxrank - mingoodrank) * reg_rank_prop),
+      dplyr::filter(scenario == 'SimR0') %>%
+      dplyr::group_by(Month) %>%
+      dplyr::filter(value >= lower_limit) %>%
+      dplyr::summarise(mingoodrank = min(ranks), maxrank = max(ranks)) %>%
+      dplyr::mutate(nrankbottom = ceiling((maxrank - mingoodrank) * reg_rank_prop),
              bottom_ranks = mingoodrank + nrankbottom)
 
 
@@ -242,10 +251,10 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
                                      monthly = monthly)
 
       rank_summary <- rank_data %>%
-        filter(scenario == 'SimR0') %>%
-        filter(value >= lower_limit) %>%
-        summarise(mingoodrank = min(ranks), maxrank = max(ranks)) %>%
-        mutate(nrankbottom = ceiling((maxrank - mingoodrank) * reg_rank_prop),
+        dplyr::filter(scenario == 'SimR0') %>%
+        dplyr::filter(value >= lower_limit) %>%
+        dplyr::summarise(mingoodrank = min(ranks), maxrank = max(ranks)) %>%
+        dplyr::mutate(nrankbottom = ceiling((maxrank - mingoodrank) * reg_rank_prop),
                bottom_ranks = mingoodrank + nrankbottom)
       # bottom_ranks <- max(rank_data$ranks, na.rm = TRUE)*reg_rank_prop
 
@@ -258,7 +267,7 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
         monthly <- TRUE
         # Cut to those ranks and above the lower values we don't trust and the baseline runoff model- differs by month
         rank_data <- rank_data %>%
-          left_join(rank_summary, by = 'Month')
+          dplyr::left_join(rank_summary, by = 'Month')
 
     }
 
@@ -279,21 +288,43 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
         regression_bottom <- lm(value ~ mod_vals, data = rank_data_0)
       }
 
+
+      # In some situations, the fits are flat enough and the realised values
+      # work out such that the predictions yield values slightly above 0 even
+      # for the baseline case where we know there are 0s, and we end up making
+      # chunks of postivie numbers. So, we can force this by finding the model
+      # values that map to zeros in the data (rank 1) for each month, and then
+      # use those as an enforced 0-limit.
+
+      if (all(rank_data_0$value > 0)) {zeroranks <- tibble(Month = unique(rank_data_0$Month), mod_limit =  NA)}
+
+      if (any(rank_data_0$value == 0)) {
+        zeroranks <- rank_data_0 %>%
+          dplyr::filter(value <= 0) %>%
+          dplyr::group_by(Month) %>%
+          dplyr::summarise(mod_limit = min(unique(mod_vals))) # Should be unique, but just in case, use the lowest
+      }
+
       bottom_rank_fit <- rank_data %>%
-        filter(ranks <= bottom_ranks) %>%
+        dplyr::filter(ranks <= bottom_ranks) %>%
         modelr::add_predictions(regression_bottom, 'pred_hyd') %>%
+        # Threshold with the zero-limit from the rankings
+        dplyr::left_join(zeroranks, by = "Month") %>%
+        # Deal with months that don't have a mod_limit (e.g. no zeros in the data)
+        dplyr::mutate(mod_limit = ifelse(is.na(mod_limit), 0, mod_limit)) %>%
+        dplyr::mutate(pred_hyd = ifelse(mod_vals <= mod_limit, 0, pred_hyd)) %>%
         # Threshold for 0- make it 1 for log, 0 for linear.
-        mutate(pred_hyd = ifelse(pred_hyd > 0, pred_hyd, 0)) %>%
+        dplyr::mutate(pred_hyd = ifelse(pred_hyd > 0, pred_hyd, 0)) %>%
         dplyr::select(Date, scenario, pred_hyd)
     }
 
     if (regression == 'log') {
       rank_data_0 <- rank_data_0 %>%
-        mutate(log_mod = log(mod_vals),
+        dplyr::mutate(log_mod = log(mod_vals),
                log_hyd = log(value))
 
       rank_data <- rank_data %>%
-        mutate(log_mod = log(mod_vals),
+        dplyr::mutate(log_mod = log(mod_vals),
                log_hyd = log(value))
 
       if (monthly) {
@@ -305,18 +336,18 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
 
 
       bottom_rank_fit <- rank_data %>%
-        filter(ranks <= bottom_ranks) %>%
+        dplyr::filter(ranks <= bottom_ranks) %>%
         modelr::add_predictions(regression_bottom, 'pred_hyd') %>%
-        mutate(pred_hyd = exp(pred_log_hyd)) %>%
+        dplyr::mutate(pred_hyd = exp(pred_log_hyd)) %>%
         # Threshold for 0- make it 1 for log, 0 for linear.
-        mutate(pred_hyd = ifelse(pred_hyd > 1, pred_hyd_from_log, 0)) %>%
+        dplyr::mutate(pred_hyd = ifelse(pred_hyd > 1, pred_hyd_from_log, 0)) %>%
         dplyr::select(Date, scenario, pred_hyd)
     }
 
     # swap the values
     qq_gauge <- qq_gauge %>%
-      left_join(bottom_rank_fit, by = c('Date', 'scenario')) %>%
-      mutate(adj_val = ifelse(is.na(pred_hyd), adj_val, pred_hyd))
+      dplyr::left_join(bottom_rank_fit, by = c('Date', 'scenario')) %>%
+      dplyr::mutate(adj_val = ifelse(is.na(pred_hyd), adj_val, pred_hyd))
 
   }
 
@@ -329,22 +360,22 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
     dplyr::mutate(scenario = 'Historical')
 
   qq_gauge <-  qq_gauge  %>%
-    dplyr::select(-pred_hyd, -matches('Month'), value = adj_val) %>%
+    dplyr::select(-matches('pred_hyd'), -matches('Month'), value = adj_val) %>%
     bind_rows(ingauge) %>%
     # pivot so the gauge name is col name
     tidyr::pivot_wider(names_from = site, values_from = value) %>%
     # collapse to a list-tibble with one row per scenario This allows pmap-ing
     # over the rows (scenarios) to save each into a different folder
-    group_by(scenario) %>%
+    dplyr::group_by(scenario) %>%
     tidyr::nest() %>%
-    ungroup()
+    dplyr::ungroup()
 
   # Save the csvs
   if (savedata) {
     purrr::pmap(qq_gauge, savefun)
   }
 
-  # Save the csvs
+  # Save the individual rdses
   if (saverds) {
     if (!dir.exists(file.path(hydro_dir, 'rds_outputs'))) {
       dir.create(file.path(hydro_dir, 'rds_outputs'),
@@ -354,7 +385,7 @@ scale_gauges <- function(gaugedata, gaugename, all_sdl_scenario_list, qc_limit =
     saveRDS(qq_gauge, file = file.path(hydro_dir, 'rds_outputs', paste0(gaugename, '.rds')))
   }
 
-  rlang::inform(glue::glue('\n\nfinished gauge {gaugename} in SDL {sdl_name}\n\n'))
+  # rlang::inform(glue::glue('\n\nfinished gauge {gaugename} in SDL {sdl_name}\n\n'))
   # Not sure this is a good idea- might want to return NULL
   # It's useful for diagnostics, I think.
 
@@ -399,16 +430,16 @@ clean_to_scale <- function(gaugedata, gaugename, sdl_runoff_scenarios, qc_limit)
   # make an NA quantile for each scenario so the join works properly
   if (any(is.na(gaugedata$value))) {
     nafill <- model_with_scaling %>%
-      distinct(scenario, Month) %>%
-      mutate(quantile = NA, mean = NA, ref_mean = NA, relative_change = NA)
+      dplyr::distinct(scenario, Month) %>%
+      dplyr::mutate(quantile = NA, mean = NA, ref_mean = NA, relative_change = NA)
 
     model_with_scaling <- bind_rows(model_with_scaling, nafill)
   }
 
   gaugedata <- gaugedata %>%
     # get the time units right
-    mutate(Month = lubridate::month(time)) %>%
-    rename(Date = time) # To match other inputs
+    dplyr::mutate(Month = lubridate::month(time, label = TRUE)) %>%
+    dplyr::rename(Date = time) # To match other inputs
 
   return(list(gaugedata = gaugedata, model_with_scaling = model_with_scaling))
 
@@ -416,7 +447,7 @@ clean_to_scale <- function(gaugedata, gaugename, sdl_runoff_scenarios, qc_limit)
 
 # simple saver for purrring over
 savefun <- function(scenario, data) {
-  write_csv(data, file = file.path(hydro_dir, scenario, paste0(names(data)[2], '.csv')))
+  readr::write_csv(data, file = file.path(hydro_dir, scenario, paste0(names(data)[2], '.csv')))
   return(invisible(NULL))
 }
 
