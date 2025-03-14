b22dfdbcb232b7dd3aa5184385ed0ba7a9171215
Galen Holt <53928785+galenholt@users.noreply.github.com>
2025-03-14 12:19:56 +1100

    withr and styler

--- workflows/params.R
+++ workflows/params.R
@@ -1,23 +1,27 @@
-# parameters that involve R objects/syntax, and so cannot be set in a yaml/json params file (yet)
-
-# The goal is to slowly work on making these able to be referenced in params.yml or json
+# parameters that involve R objects/syntax cannot be set in a yaml/json params
+# file. It is better to use characters,but sometimes that's not possible
 
 # Aggregation sequence
-aggseq <- list(ewr_code = c('ewr_code_timing', 'ewr_code'),
-               env_obj =  c('ewr_code', "env_obj"),
-               sdl_units = HydroBOT::sdl_units,
-               Specific_goal = c('env_obj', "Specific_goal"),
-               Objective = c('Specific_goal', 'Objective'),
-               mdb = HydroBOT::basin,
-               target_5_year_2024 = c('Objective', 'target_5_year_2024'))
+aggseq <- list(
+  ewr_code = c("ewr_code_timing", "ewr_code"),
+  env_obj = c("ewr_code", "env_obj"),
+  sdl_units = HydroBOT::sdl_units,
+  Specific_goal = c("env_obj", "Specific_goal"),
+  Objective = c("Specific_goal", "Objective"),
+  mdb = HydroBOT::basin,
+  target_5_year_2024 = c("Objective", "target_5_year_2024")
+)
 
 # Functions for each aggregation
-funseq <- list(c('CompensatingFactor'),
-               c('ArithmeticMean'),
-               c('ArithmeticMean'),
-               c('ArithmeticMean'),
-               c('ArithmeticMean'),
-
-               rlang::quo(list(wm = ~weighted.mean(., w = area,
-                                                   na.rm = TRUE))),
-               c('ArithmeticMean'))
+funseq <- list(
+  c("CompensatingFactor"),
+  c("ArithmeticMean"),
+  c("ArithmeticMean"),
+  c("ArithmeticMean"),
+  c("ArithmeticMean"),
+  rlang::quo(list(wm = ~ weighted.mean(.,
+    w = area,
+    na.rm = TRUE
+  ))),
+  c("ArithmeticMean")
+)
