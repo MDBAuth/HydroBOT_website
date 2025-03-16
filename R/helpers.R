5a7b6eab70fa51633955463c02e81342246ae529
Galen Holt <53928785+galenholt@users.noreply.github.com>
2025-03-17 09:20:47 +1100

    styling, some aggregation

--- R/helpers.R
+++ R/helpers.R
@@ -1,38 +1,38 @@
-
 make_hydro_csv <- function() {
-
-  if (!dir.exists('hydrobot_scenarios')) {
-    dir.create('hydrobot_scenarios')
+  if (!dir.exists("hydrobot_scenarios")) {
+    dir.create("hydrobot_scenarios")
   }
 
-  file.copy(system.file('extdata/testsmall/hydrographs', package = 'HydroBOT'),
-            'hydrobot_scenarios', recursive = TRUE)
+  file.copy(system.file("extdata/testsmall/hydrographs", package = "HydroBOT"),
+    "hydrobot_scenarios",
+    recursive = TRUE
+  )
 
   # This destroys it once used
-  withr::defer_parent(unlink('hydrobot_scenarios', recursive = TRUE))
-
+  withr::defer_parent(unlink("hydrobot_scenarios", recursive = TRUE))
 }
 
 make_hydro_nc <- function() {
-
-  if (!dir.exists('hydrobot_netcdf')) {
-    dir.create('hydrobot_netcdf')
+  if (!dir.exists("hydrobot_netcdf")) {
+    dir.create("hydrobot_netcdf")
   }
 
-  file.copy(system.file('extdata/ncdfexample/nchydros', package = 'HydroBOT'),
-            'hydrobot_netcdf', recursive = TRUE)
-  file.copy(system.file('extdata/ncdfexample/zipcdf.zip', package = 'HydroBOT'),
-            'hydrobot_netcdf', recursive = TRUE)
+  file.copy(system.file("extdata/ncdfexample/nchydros", package = "HydroBOT"),
+    "hydrobot_netcdf",
+    recursive = TRUE
+  )
+  file.copy(system.file("extdata/ncdfexample/zipcdf.zip", package = "HydroBOT"),
+    "hydrobot_netcdf",
+    recursive = TRUE
+  )
 
   # This destroys it once used
-  withr::defer_parent(unlink('hydrobot_scenarios', recursive = TRUE))
-
+  withr::defer_parent(unlink("hydrobot_scenarios", recursive = TRUE))
 }
 
 
-make_simpleyml <- function(renderfile = 'auto') {
-
-  if (renderfile == 'auto') {
+make_simpleyml <- function(renderfile = "auto") {
+  if (renderfile == "auto") {
     if (rstudioapi::isAvailable()) {
       projpath <- rstudioapi::getActiveProject()
       docpath <- rstudioapi::documentPath()
@@ -43,17 +43,11 @@ make_simpleyml <- function(renderfile = 'auto') {
       rlang::inform("Rstudio not running, do not want new profiles created while rendering, skipping")
       return(invisible())
     }
-
-
   }
 
 
   simple_yaml <- list()
   simple_yaml$project <- list()
   simple_yaml$project$render <- list(renderfile)
-  yaml::write_yaml(simple_yaml, '_quarto-singlefile.yml')
+  yaml::write_yaml(simple_yaml, "_quarto-singlefile.yml")
 }
-
-
-
-
